21108d44719baa58f7a44b7008e01a008282c56e
cyberfunk <hello@cyberfunk.org>
2014-09-27 00:51:42 +0800

    Initial files.

---
+++ test/spec/system.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var sys = require('../..');
+
+describe('cli-system:', function() {
+  it('should execute middleware function', function(done) {
+    done();
+  });
+})
